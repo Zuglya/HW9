d69500a7251d004a27f5e2e5a0a8eff90d5221c4
Zuglya <irussel86@gmail.com>
2024-09-24 22:39:47 +0400

    Решено ДЗ Урок9 ДЗ1

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
@@ -15,22 +17,32 @@ public class Main {
 
     private static void divide() {
         //todo дописать логику и печать результата
+        if(b != 0) {
+            System.out.println("Частное a/b= " + (a / b));
+        } else {
+            System.out.println("Делить на ноль нельзя");
+            return;
+        }
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        System.out.println("Разность a-b= " + (a - b));
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        System.out.println("Произведение a*b= " + (a * b));
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        System.out.println("Сумма a+b= " + (a + b));
     }
 
     private static double inputDouble() {
         //todo дописать логику
-        return 0;
+        System.out.println("Введите число:");
+        return new Scanner(System.in).nextDouble();
     }
 }
