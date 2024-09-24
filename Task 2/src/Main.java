3c3fb6690b52e7e1fe9c4e64ca92a53b2324f5af
Zuglya <irussel86@gmail.com>
2024-09-24 22:51:09 +0400

    Решение ДЗ Урок9 Задание2

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        isEven();
+    }
+
+    private static void isEven() {
+        if(inputInt() % 2 == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+    private static int inputInt() {
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextInt();
     }
 }
